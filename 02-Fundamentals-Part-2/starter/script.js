5fd419eb60e2aecabf36ed9e624c40551f17a8a1
Matt Hancock <42810039+mgh3x@users.noreply.github.com>
2021-08-03 10:19:25 -0400

    work

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*let hasDriversLicense = false;
+const passTest = true;
+
+//if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive');*/
+
+/*function logger() {
+    console.log('Yes');
+}
+
+logger();
+
+*/
